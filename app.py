4119830e840fbe0a32b0c2ded95ae67c769544fe
Sopan30 <SOPAN.TAMBEKAR@HONEYWELL.COM>
2026-07-18 13:02:52 +0530

    Add stage selection for Multi-Side Stream Compressor

--- app.py
+++ app.py
@@ -15,6 +15,8 @@ from xml.sax.saxutils import escape
 st.set_page_config(page_title='Compressor Curve Regression', layout='wide')
 st.title('Compressor Curve Regression Tool')
 compressor_type = st.radio("Select Compressor Type",["Centrifugal Compressor", "Multi-Side Stream Compressor"],horizontal=True)
+if compressor_type == "Multi-Side Stream Compressor":
+    no_of_stages=st.selectbox(label="Choose number of stages",options=list(range(1,11)))
 
 method = st.sidebar.selectbox('Regression Method',
     ['Auto Best Fit','Linear','Quadratic','Cubic','4th Order','5th Order','Spline'])
